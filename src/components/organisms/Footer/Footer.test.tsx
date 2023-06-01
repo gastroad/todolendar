4cdacfce7a9ec32fb7193ef6e1507d07ae4c2d15
진현성 <gostroad37@gmail.com>
2023-06-02 04:35:40 +0900

    Footer,Header test code 추가

---
+++ src/components/organisms/Footer/Footer.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+
+test('renders footer', () => {
+  render(<Footer />);
+  const footerElement = screen.getByRole('footer');
+  expect(footerElement).toBeInTheDocument();
+});
