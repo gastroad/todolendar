ef8ef898b9b2b86068026c5eae51bf69dc31033d
진현성 <gostroad37@gmail.com>
2023-06-02 19:27:01 +0900

    organism/Header 에  NavigationMenu 추가 및 Header 스타일 적용

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
 import Logo from '@atoms/Logo';
 
+import NavigationMenu from '@molecules/NavigationMenu';
+import "./Header.scss"
+
 const Header: FC = () => {
   return (
-    <header>
-      <Logo />
+    <header id='header'>
+      <h1><Logo /></h1>
+      <NavigationMenu />
     </header>
   );
 };
