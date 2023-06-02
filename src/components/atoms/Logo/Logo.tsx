f45966860ada3b5cfed12e112cd1e2b8d472d12c
진현성 <gostroad37@gmail.com>
2023-06-02 19:20:21 +0900

    logo 이미지 및 스타일 변경

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 
 import LogoURL from '../../../assets/images/logo.png';
+import "./Logo.scss"
 
 const Logo: FC = () => {
-  return <img src={LogoURL} />;
+  return <img id='logo' src={LogoURL} />;
 };
 
 export default Logo;
