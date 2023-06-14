442149c4e3ecd256efbf55d52c723602108a79bc
진현성 <gostroad37@gmail.com>
2023-06-15 03:02:31 +0900

    templates/TodolendarTemplate 추가

--- src/templates/TodolendarTemplate/TodolendarTemplate.tsx
+++ src/templates/TodolendarTemplate/TodolendarTemplate.tsx
@@ -1,21 +1,51 @@
 import { FC } from 'react';
+import { createPortal } from 'react-dom';
+import { DateTime } from 'luxon';
+
 import Calendar from '@organisms/Calendar';
+import TodoListModal from '@organisms/TodoListModal';
+import SectionTitle from '@atoms/SectionTitle';
+import type { Todo } from '@src/types/todoList';
 
-const TodolendarTemplate: FC = () => {
+import './TodolendarTemplate.scss';
+
+interface TodolendarTemplateProps {
+  currentDate: DateTime;
+  setCurrentDate: (currentDate: DateTime) => void;
+  todos: Todo[];
+  onDateSelect: (currentDate: DateTime) => void;
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+const TodolendarTemplate: FC<TodolendarTemplateProps> = ({
+  currentDate,
+  todos = [],
+  onDateSelect,
+  isOpen,
+  openModal,
+  closeModal,
+}) => {
   return (
-    <section>
-      <article>
-        <Calendar
-          onDateSelect={(date) => {
-            console.log(date);
-          }}
-        />
-      </article>
-      <article>
-        {/* todolist */}
-      </article>
-
-    </section >
+    <>
+      <section className="section">
+        <SectionTitle title="Planner" />
+        <div className="calendar-wrapper">
+          <Calendar onDateSelect={onDateSelect} />
+        </div>
+      </section>
+      {isOpen &&
+        createPortal(
+          <TodoListModal
+            currentDate={currentDate}
+            todos={todos}
+            openModal={openModal}
+            closeModal={closeModal}
+          />,
+          document.getElementById('root-modal') as HTMLElement,
+        )}
+    </>
   );
 };
 
