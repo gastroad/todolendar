2e5a7387f2071a15a5391d74d73ec3499686b7e2
진현성 <gostroad37@gmail.com>
2023-06-15 02:11:14 +0900

    molecules/navigationMenu active 상태 추가

--- src/components/molecules/NavigationMenu/NavigationMenu.tsx
+++ src/components/molecules/NavigationMenu/NavigationMenu.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationMenu.scss';
+import { useLocation } from 'react-router-dom';
 
 export const NavigationMenuList: { link: string; label: string }[] = [
   { link: 'dashbaord', label: 'DashBaord' },
@@ -8,18 +9,20 @@ export const NavigationMenuList: { link: string; label: string }[] = [
 ];
 
 const NavigationMenu: FC = () => {
+  const location = useLocation();
+
   return (
     <nav id="nav" data-testid="nav">
       <ul>
         {NavigationMenuList.map(({ link, label }) => {
           return (
             <li key={link}>
-              <Link to={link}>{label}</Link>
+              <Link className={`${Boolean(location.pathname.includes(link)) && "active"}`} to={link}>{label}</Link>
             </li>
           );
         })}
       </ul>
-    </nav>
+    </nav >
   );
 };
 export default NavigationMenu;
