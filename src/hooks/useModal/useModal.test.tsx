bd296bdccd50617d49abe8118cc134a2e30c5a33
진현성 <gostroad37@gmail.com>
2023-06-15 03:01:59 +0900

    hooks/useModal 추가

---
+++ src/hooks/useModal/useModal.test.tsx
@@ -0,0 +1,41 @@
+import { render, fireEvent, act } from '@testing-library/react';
+import useModal from './useModal';
+
+describe('useModal', () => {
+  const TestComponent = () => {
+    const { isOpen, openModal, closeModal } = useModal();
+    return (
+      <div>
+        <button onClick={openModal}>Open Modal</button>
+        {isOpen && <div className="modal-content" data-testid="modal-content">Modal Content</div>}
+        <button onClick={closeModal}>Close Modal</button>
+      </div>
+    );
+  };
+
+  test('opens and closes modal', () => {
+    const { getByText, queryByText } = render(<TestComponent />);
+    expect(queryByText('Modal Content')).toBeNull();
+    fireEvent.click(getByText('Open Modal'));
+    expect(queryByText('Modal Content')).toBeInTheDocument();
+    fireEvent.click(getByText('Close Modal'));
+    expect(queryByText('Modal Content')).toBeNull();
+  });
+
+  test('closes modal on escape key press', () => {
+    const { getByText, queryByTestId } = render(<TestComponent />);
+    // 확인필요
+    fireEvent.click(getByText('Open Modal'));
+    expect(queryByTestId('modal-content')).toBeInTheDocument();
+    fireEvent.keyDown(document, { key: 'Escape' })
+    expect(queryByTestId('modal-content')).toBeNull();
+  });
+
+  test('closes modal on outside click', () => {
+    const { getByText, queryByText } = render(<TestComponent />);
+    fireEvent.click(getByText('Open Modal'));
+    expect(queryByText('Modal Content')).toBeInTheDocument();
+    fireEvent.mouseDown(document);
+    expect(queryByText('Modal Content')).toBeNull();
+  });
+});
---
+++ src/hooks/useModal/useModal.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+
+const useModal = (): {
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+} => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const closeModalOnOutsideClick = (e: MouseEvent) => {
+      const modalContent = document.querySelector('.modal-content');
+      if (modalContent && !modalContent.contains(e.target as Node)) {
+        closeModal();
+      }
+    };
+
+    const bindEscapeToClose = (e: KeyboardEvent) => {
+      console.log(e.code)
+      if (e.code === 'Escape') {
+        closeModal();
+      }
+    };
+
+    window.addEventListener('keydown', bindEscapeToClose);
+    document.addEventListener('mousedown', closeModalOnOutsideClick);
+
+    return () => {
+      window.removeEventListener('keydown', bindEscapeToClose);
+      document.removeEventListener('mousedown', closeModalOnOutsideClick);
+    };
+  }, []);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return { isOpen, openModal, closeModal };
+};
+
+export default useModal;
