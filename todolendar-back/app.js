32fb10f00997be1e36a4abc7d42823274b3a88fb
진현성 <gostroad37@gmail.com>
2023-06-15 20:35:22 +0900

    todolist 조회 생성 수정 삭제 api 생성

--- todolendar-back/app.js
+++ todolendar-back/app.js
@@ -15,10 +15,78 @@ function saveData(data) {
     fs.writeFileSync(dataFilePath, json);
 }
 
-app.get('/api/data', (req, res) => {
+app.get('/api/todolist', (req, res) => {
+    const year = req.query.year;
+    const month = req.query.month;
+
+    if (!year && !month) {
+        const data = loadData();
+        res.json(data);
+        return;
+    }
+
     const data = loadData();
-    res.json(data);
+    let filteredTodoList = data;
+
+    if (year) {
+        filteredTodoList = filteredTodoList.filter(item => {
+            const itemYear = item.date.substr(0, 4);
+            return itemYear === year;
+        });
+    }
+
+    if (month) {
+        filteredTodoList = filteredTodoList.filter(item => {
+            const itemMonth = item.date.substr(5, 2);
+            return itemMonth === month;
+        });
+    }
+
+    res.json(filteredTodoList);
 });
+
+
+app.post('/api/todolist', (req, res) => {
+    const { text, completed, date } = req.body;
+
+    const data = loadData();
+    const newTodo = {
+        id: data.length + 1,
+        text,
+        completed,
+        date
+    };
+    data.push(newTodo);
+    saveData(data);
+
+    res.json({ message: 'Todo item added successfully' });
+});
+
+app.put('/api/todolist/:id', (req, res) => {
+    const id = req.params.id;
+    const { text, completed, date } = req.body;
+
+    const data = loadData();
+    const todo = data.find(item => item.id === parseInt(id));
+    todo.text = text;
+    todo.completed = completed;
+    todo.date = date;
+    saveData(data);
+
+    res.json({ message: 'Todo item updated successfully' });
+});
+
+app.delete('/api/todolist/:id', (req, res) => {
+    const id = req.params.id;
+    const data = loadData();
+
+    const index = data.findIndex(item => item.id === parseInt(id));
+    data.splice(index, 1);
+    saveData(data);
+
+    res.json({ message: 'Todo item deleted successfully' });
+});
+
 
 
 app.listen(PORT, () => {
