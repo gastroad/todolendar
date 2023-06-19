900206cd45aa69023a87462e53cbbd21d8becbf0
진현성 <gostroad37@gmail.com>
2023-06-20 02:59:03 +0900

    react-query, recoil 설치

--- todolendar-front/src/index.tsx
+++ todolendar-front/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 
 import router from '@src/router';
 
@@ -8,8 +10,14 @@ import 'core-js';
 import 'regenerator-runtime/runtime';
 import '@assets/styles/reset.scss';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <RouterProvider router={router} />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
