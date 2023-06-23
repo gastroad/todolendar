37cd0babe6835112f64af5cb8a17f7c0c345ff54
진현성 <gostroad37@gmail.com>
2023-06-23 10:11:25 +0900

    불필요한 코드 정리

--- todolendar-front/src/components/organisms/Header/Header.test.tsx
+++ todolendar-front/src/components/organisms/Header/Header.test.tsx
@@ -4,14 +4,12 @@ import { MemoryRouter } from 'react-router-dom';
 
 describe('Header', () => {
   it('renders header component', () => {
-    // Header 컴포넌트 렌더링
     const { getByTestId } = render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>,
     );
 
-    // Header 컴포넌트가 렌더링되었는지 확인
     const headerElement = getByTestId('header');
     expect(headerElement).toBeInTheDocument();
   });
