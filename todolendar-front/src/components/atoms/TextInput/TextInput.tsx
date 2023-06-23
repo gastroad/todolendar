a5769aa2867b3736f83f4fa2e386c5ab7dddf07f
진현성 <gostroad37@gmail.com>
2023-06-23 13:00:14 +0900

    atoms/TextInput type 추가

--- todolendar-front/src/components/atoms/TextInput/TextInput.tsx
+++ todolendar-front/src/components/atoms/TextInput/TextInput.tsx
@@ -7,17 +7,19 @@ export interface TextInputProps {
   placeholder?: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   onKeyDown?: (e: KeyboardEvent<HTMLInputElement>) => void;
+  type?: string
 }
 const TextInput: FC<TextInputProps> = ({
   value,
   placeholder = '',
   onChange,
   onKeyDown,
+  type = "text"
 }) => {
   return (
     <input
       className="input-txt"
-      type="text"
+      type={type}
       value={value}
       onChange={onChange}
       onKeyDown={onKeyDown}
