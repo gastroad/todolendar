f914635afcab67f3b2890302d096068d6d29bc10
진현성 <gostroad37@gmail.com>
2023-06-23 10:12:46 +0900

    page/DashboardPage 추가

--- todolendar-front/src/pages/DashboardPage/DashboardPage.tsx
+++ todolendar-front/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,11 +1,29 @@
 import { FC } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Todo } from '@src/types/todoList';
+import { DateTime } from 'luxon';
+import { httpGetTodos } from '@src/api/planner';
 import DashboardTemplate from '@templates/DashboardTemplate';
 
 export interface DashboardPageProps {}
 const DashboardPage: FC<DashboardPageProps> = () => {
+  const getTodosQuery = useQuery<Todo[], Error>({
+    queryKey: ['todoList'],
+    queryFn: () =>
+      httpGetTodos({
+        year: DateTime.now().toFormat('yyyy'),
+      }),
+  });
+
+  if (getTodosQuery.isLoading) {
+    return <>로딩중</>;
+  }
+  if (getTodosQuery.isError) {
+    return <>에러 발생</>;
+  }
   return (
     <>
-      <DashboardTemplate />
+      <DashboardTemplate todos={getTodosQuery.data} />
     </>
   );
 };
